eea42dfb7d3d096da9b27642544d8585f9581289
JP Doherty <jpdoherty90@gmail.com>
2017-06-25 21:35:00 -0500

    added bonus functionality

--- practice.js
+++ practice.js
@@ -1,16 +1,19 @@
-
-function slotMachine(quarters) {
+function slotMachine(quarters, leaveWith) {
     while (quarters > 0) {
+        quarters--;
         var winningNumber = Math.floor(Math.random() * 100) + 1;
         var yourNumber = Math.floor(Math.random() * 100) + 1;
         if (winningNumber === yourNumber) {
             var winnings = Math.floor(Math.random() * 51) + 50;
             quarters += winnings;
-            return quarters;
+            if (quarters >= leaveWith) {
+                return quarters;
+            } else {
+                continue;
+            }
         }
-        quarters--;
     }
     return 0;
 }
 
-console.log(slotMachine(100));
+console.log(slotMachine(100, 119));
